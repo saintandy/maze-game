37f6899505868e1c9a3a9a2b2f46c5ff4f2cce3b
vlad <vladtarniceru@gmail.com>
2014-04-11 17:51:54 +0300

    added maze.js

--- maze.js
+++ maze.js
@@ -37,52 +37,53 @@ function inside(node) {
 	return false;
 }
 
-function dfs(node, move) {
-	var where = Math.random();
-	
-	if (!inside(node)) // ai ajuns la un capat, deci ti-ai facut drumul
-		return ;
-	fr[node.x][node.y] = move;
-	
-	if (where <= 0.25)
-		node.x++;
-	else
-	if (where <= 0.5)
-		node.x--;
-	else
-	if (where <= 0.75)
-		node.y++;
-	else
-		node.y--;
-	dfs(node, move + 1);
-}
-
-function abs(int a) {
-	if (a < 0)
-		return -a;
-	return a;
-}
-
-// ideea de aici e ca incerc sa fac un dfs care sa ajunga cu bine la unul din capete pentru ca garanta ca exista solutie
-// si ma duc random si cam asta e :))
-
-var init = {x: 12, y: 12}; // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-dfs(init, 1);
+// la inceput presupun ca toate sunt puse
 
 var hrLines = new Array(height/blockSize + 1);
 for(var i = 0; i < hrLines.length; ++i) {
 	hrLines[i] = new Array(width/blockSize);
 	for(var j = 0; j < hrLines[i].length; ++j)
-		hrLines[i][j] = abs(fr[i][j - 1] && fr[i][j]) == 1 ? false : (Math.random() <= 0.4 ? false : true);
-		// aici si la vrLines e bugul de nu-ti printeaza aia, dar nu-mi dau seama de ce
-		// eu vreau sa verific daca i, j - 1 is i, j sunt consecutive ca sa nu pun zid intre ele
+		hrLines[i][j] = true;
 }
 var vrLines = new Array(height/blockSize); // nu e + 1 aici?
 for(var i = 0; i < vrLines.length; ++i) {
 	vrLines[i] = new Array(width/blockSize + 1);
 	for(var j = 0; j < vrLines[i].length; ++j)
-		vrLines[i][j] = abs(fr[i][j], fr[i][1 + j]) == 1 ? false : (Math.random() <= 0.4 ? false : true);
+		vrLines[i][j] = true;//fr[i][j] && fr[i][j] ? false : (Math.random() <= 0.4 ? false : true);
 }
+
+function dfs(node) {
+	var where = Math.random();
+	
+	if (!inside(node)) // ai ajuns la un capat, deci ti-ai facut drumul
+		return ;
+	
+	if (where <= 0.25) {
+		node.x++;
+		hrLines[node.y][node.x] = true;
+	}
+	else
+	if (where <= 0.5) {
+		node.x--;
+		hrLines[node.y][node.x] = true;
+	}
+	else
+	if (where <= 0.75) {
+		node.y++;
+		vrLines[node.y][node.x] = true;
+	}
+	else {
+		node.y--;
+		hrLines[node.y][node.x] = true; // niciuna din astea nu e buna, dar intelegi tu ce vreau sa zic
+		// incerc sa scot barele dintre astea 2 care sunt consecutive
+		// incearca sa le scrii tu, eu nu inteleg cum iti faci coordonatele sincer
+	}
+	
+	dfs(node);
+}
+
+var init = {x: 12, y: 12}; // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+dfs(init);
 
 var player = {x:0, y:0}; // aici incearca sa plecam din mijloc, adica de la 12 12
 // oricum cred ca nu e buna functia DrawPlayer
